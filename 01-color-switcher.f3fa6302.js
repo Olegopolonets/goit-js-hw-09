e3307acc3c46a770dbab70293c622affda38336c
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-09 16:05:19 +0000

    Deploying to gh-pages from @ Olegopolonets/goit-js-hw-09@13f81f89b67499624f43b36161e116bbf3007063 🚀

---
+++ 01-color-switcher.f3fa6302.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(e),t.addEventListener("click",(function(){timerColor=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.setAttribute("disabled",!0),e.removeAttribute("disabled",!1)}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerColor),t.removeAttribute("disabled",!0),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.f3fa6302.js.map
