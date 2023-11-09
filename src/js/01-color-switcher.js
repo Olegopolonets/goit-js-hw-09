93858a000614e5f25fa4cef3747544c372afd66a
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-09 02:03:19 +0200

    complated 1 task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled', false);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerColor);
+  startBtn.removeAttribute('disabled', true);
+  stopBtn.setAttribute('disabled', true);
+});
