bcd5041e0cf624684283811887bf8145f18a189c
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-11-12 22:23:08 +0200

    add fireworks in task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,59 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Fireworks } from 'fireworks-js';
 
+const container = document.querySelector('.container');
+const stopFireworks = document.querySelector('.stop-fireworks');
+const fireworks = new Fireworks(container, {
+  autoresize: true,
+  opacity: 0.5,
+  acceleration: 1.05,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 50,
+  traceLength: 3,
+  traceSpeed: 10,
+  explosion: 5,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round',
+  hue: {
+    min: 0,
+    max: 360,
+  },
+  delay: {
+    min: 30,
+    max: 60,
+  },
+  rocketsPoint: {
+    min: 50,
+    max: 50,
+  },
+  lineWidth: {
+    explosion: {
+      min: 1,
+      max: 3,
+    },
+    trace: {
+      min: 1,
+      max: 2,
+    },
+  },
+  brightness: {
+    min: 50,
+    max: 80,
+  },
+  decay: {
+    min: 0.015,
+    max: 0.03,
+  },
+  mouse: {
+    click: false,
+    move: false,
+    max: 1,
+  },
+});
 const input = document.querySelector('#datetime-picker');
 const start = document.querySelector('button[data-start]');
 const daysOutput = document.querySelector('span[data-days]');
@@ -51,8 +103,7 @@ function startTimer() {
     const sumDateSeconds = Object.values(currentTime).reduce((a, b) => a + b, 0);
     if (sumDateSeconds < 0) {
       clearInterval(timerInterval);
-      Notify.success('Please choose a date in the future');
-
+      fireworks.start();
       return;
     }
 
@@ -60,6 +111,7 @@ function startTimer() {
     minutesOutput.textContent = addLeadingZero(currentTime.minutes);
     hoursOutput.textContent = addLeadingZero(currentTime.hours);
     daysOutput.textContent = addLeadingZero(currentTime.days);
+    fireworks.stop();
   }, 1000);
 }
 
@@ -69,3 +121,7 @@ function addLeadingZero(value) {
 }
 
 start.addEventListener('click', startTimer);
+// stopFireworks.addEventListener('click', () => {
+//   console.log('112');
+
+// });
